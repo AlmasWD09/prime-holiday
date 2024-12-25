39c4b28907ae60847afa9cddef9b694d7670ecff
AlmasWD09 <almashossain7384@gmail.com>
2024-12-25 15:08:54 +0600

    cloudneary add .....

--- src/app/(pages)/cloudenary/page.js
+++ src/app/(pages)/cloudenary/page.js
@@ -7,7 +7,7 @@ const page = () => {
     const [hostedUrl, setHostedUrl] = useState('')
   return (
     <div className="flex flex-col space-y-4 justify-center items-center h-screen">
-        <p><span className="font-bold text-green-400 pr-10">URL: </span> {hostedUrl}</p>
+        <p><span className="font-bold text-green-400 pr-12">URL: </span> {hostedUrl}</p>
       <CloudenaryImageUpload setHostedUrl={setHostedUrl}/>
     </div>
   )
